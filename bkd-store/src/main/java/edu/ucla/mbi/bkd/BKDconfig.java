81516a16c95ddc20a3dc3f7c316b4a1614f442b5
Lukasz Salwinski <lukasz@mbi.ucla.edu>
2021-09-23 19:24:19 -0700

    missing file added

---
+++ bkd-store/src/main/java/edu/ucla/mbi/bkd/BKDconfig.java
@@ -0,0 +1,20 @@
+package edu.ucla.mbi.bkd;
+
+public class BKDconfig{
+ 
+    private static String prefix = "BKD";
+    
+    public BKDconfig(){}
+    
+    public void setPrefix( String value ){
+        prefix = value;
+    }
+
+    public static String getPrefix(){
+        return prefix;
+    }
+
+    
+    
+    
+}
